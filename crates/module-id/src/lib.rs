8bc3f8ad37b775056078e06f21a1b44f18155b89
Ian Macalinao <ian@moving.wtf>
2022-07-03 15:02:07 -0500

    more tests

--- crates/module-id/src/lib.rs
+++ crates/module-id/src/lib.rs
@@ -1,15 +1,31 @@
 //! Wrapper type for serializing Move module IDs as strings.
 
+use anyhow::Result;
 use move_core_types::{
     language_storage::{ModuleId, StructTag},
     parser::parse_struct_tag,
 };
 use serde::{Deserialize, Serialize, Serializer};
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
-/// Wrapper around [ModuleId] which can be serialized to JSON.
+/// Wrapper around [ModuleId] which is serialized as a string.
 #[derive(Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
 pub struct ModuleIdData(ModuleId);
+
+impl ModuleIdData {
+    /// Renders the [ModuleIdData] as a short string.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use module_id::*;
+    /// let id: ModuleIdData = module_id::parse_module_id("0x1::Errors").unwrap();
+    /// assert_eq!("0x1::Errors", id.short_str_lossless());
+    /// ```
+    pub fn short_str_lossless(&self) -> String {
+        self.0.short_str_lossless()
+    }
+}
 
 impl Display for ModuleIdData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -56,10 +72,30 @@ impl<'de> Deserialize<'de> for ModuleIdData {
         D: serde::Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        let tt: StructTag =
-            parse_struct_tag(&format!("{}::Dummy", &s)).map_err(serde::de::Error::custom)?;
+        ModuleIdData::from_str(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl FromStr for ModuleIdData {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let tt: StructTag = parse_struct_tag(&format!("{}::Dummy", s))?;
         Ok(ModuleIdData(tt.module_id()))
     }
+}
+
+/// Parses a module ID.
+///
+/// # Example
+///
+/// ```
+/// use move_core_types::language_storage::ModuleId;
+/// let id: ModuleId = module_id::parse_module_id("0x1::Errors").unwrap().into();
+/// assert_eq!("0x1::Errors", id.short_str_lossless());
+/// ```
+pub fn parse_module_id(raw: &str) -> Result<ModuleIdData> {
+    ModuleIdData::from_str(raw)
 }
 
 #[cfg(test)]
@@ -75,6 +111,6 @@ mod tests {
         let ser = serde_json::to_string(&my_module_id).unwrap();
         let des: ModuleIdData = serde_json::from_str(&ser).unwrap();
 
-        println!("{}", &des);
+        assert_eq!(my_module_id, des);
     }
 }
