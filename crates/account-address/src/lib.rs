fcc5477a745e467ac33c176e3bfda56ef2ec431a
Ian Macalinao <ian@moving.wtf>
2022-08-13 04:37:54 -0500

    ensure all features are present

--- crates/account-address/src/lib.rs
+++ crates/account-address/src/lib.rs
@@ -10,7 +10,7 @@ use serde::{Deserialize, Serialize, Serializer};
 use std::{fmt::Display, ops::Deref, str::FromStr};
 
 /// Wrapper around [AccountAddress] which is serialized as a string.
-#[derive(Copy, Debug, PartialEq, Hash, Eq, Clone, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AccountAddressData(AccountAddress);
 
 impl JsonSchema for AccountAddressData {
